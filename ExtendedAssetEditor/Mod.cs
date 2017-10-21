154db44219af7f0b599d7e2c875d685a32182635
Acc3ssViolation <wrdbrookhuis@gmail.com>
2017-10-21 11:39:34 +0200

    Green Cities DLC update (0.4.6)
    Updated libraries, no other changes were required.

--- ExtendedAssetEditor/Mod.cs
+++ ExtendedAssetEditor/Mod.cs
@@ -5,7 +5,7 @@ namespace ExtendedAssetEditor
     public class Mod : IUserMod
     {
         public const string name = "Extended Asset Editor";
-        public const string versionString = "0.4.5a";
+        public const string versionString = "0.4.6";
 
         public string Description
         {
