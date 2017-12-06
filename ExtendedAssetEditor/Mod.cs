45f74ed73aa21b40f87b32a8af44e41042dd6129
Acc3ssViolation <wrdbrookhuis@gmail.com>
2017-12-06 12:11:13 +0100

    Fixes compatibility with CS 1.9.1
    - Removed libraries from repo, recommend linking to the game files
    directly
    - Upped version number
    - Rebuilding mod fixes missing method errors.

--- ExtendedAssetEditor/Mod.cs
+++ ExtendedAssetEditor/Mod.cs
@@ -5,7 +5,7 @@ namespace ExtendedAssetEditor
     public class Mod : IUserMod
     {
         public const string name = "Extended Asset Editor";
-        public const string versionString = "0.4.6";
+        public const string versionString = "0.4.7";
 
         public string Description
         {
