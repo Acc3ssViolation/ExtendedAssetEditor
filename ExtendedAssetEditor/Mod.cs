cd3454e0edc4c9ff9fd383bd6701e5396ca90afe
Acc3ssViolation <wrdbrookhuis@gmail.com>
2018-05-25 20:41:17 +0200

    Updates version to 0.4.8
    For the parklife update only a recompile against the new assemblies was
    required.

--- ExtendedAssetEditor/Mod.cs
+++ ExtendedAssetEditor/Mod.cs
@@ -5,7 +5,7 @@ namespace ExtendedAssetEditor
     public class Mod : IUserMod
     {
         public const string name = "Extended Asset Editor";
-        public const string versionString = "0.4.7a";
+        public const string versionString = "0.4.8";
 
         public string Description
         {
