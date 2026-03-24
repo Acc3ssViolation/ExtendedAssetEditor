7a7e683f463fa5c8b0fd47be8d207120cc9bf2a1
Acc3ss Violation <wrdbrookhuis@gmail.com>
2026-03-24 20:49:35 +0100

    Bump version to 0.7.7

--- ExtendedAssetEditor/Mod.cs
+++ ExtendedAssetEditor/Mod.cs
@@ -5,7 +5,7 @@ namespace ExtendedAssetEditor
     public class Mod : IUserMod
     {
         public const string ModName = "Extended Asset Editor";
-        public const string VersionString = "0.7.6";
+        public const string VersionString = "0.7.7";
         public const string HarmonyPackage = "com.github.accessviolation.eae";
 
         public string Description
